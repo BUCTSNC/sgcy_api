5560b5c145931c975595c8519818b97bfc43f60e
Hexiang Qi <qihexiang@outlook.com>
2022-03-03 22:17:02 +0800

    将刷新数据库的接口独立出去

--- server.ts
+++ server.ts
@@ -1,5 +1,4 @@
 import {
-    compute,
     createRes,
     createSwRtX,
     EntryPoint,
@@ -7,41 +6,26 @@ import {
     parseURL,
     shimHTTP,
 } from "https://deno.land/x/freesia@v1.0.6/mod.ts";
+import { join } from "https://deno.land/std@0.127.0/path/mod.ts";
 import { serve } from "https://deno.land/std@0.127.0/http/server.ts";
 import { Status } from "https://deno.land/std@0.127.0/http/http_status.ts";
 import { initDB } from "./memoryDB/db.ts";
 import fileHandler from "./handlers/fileHandler.ts";
 import searchHandler from "./handlers/serachHandler.ts";
-import { join } from "https://deno.land/std@0.127.0/path/mod.ts";
+import refreshDBHandler from "./handlers/refreshDB.ts";
 
-const token = crypto.randomUUID();
+export const token = crypto.randomUUID();
+export const root = join(Deno.cwd(), "docs");
 
 const { switcher } = createSwRtX
     .route("/file/<uuid>/<filepath>", Get(fileHandler))
     .route("/search", Get(searchHandler))
-    .route(
-        "/update_db",
-        Get((_, req) => {
-            return compute(parseURL(req).searchParams.get("token"))
-                .map((value) => value === token)
-                .map(async (result) => {
-                    if (result) {
-                        const amount = await initDB();
-                        return createRes(
-                            `${amount} post${amount > 1 ? "s" : ""} detected`,
-                        );
-                    }
-                    return createRes(Status.Forbidden, "无效的令牌");
-                })
-                .value;
-        }),
-    )
+    .route("/refresh", Get(refreshDBHandler))
     .fallback(async (url, req) =>
         createRes(Status.NotFound, `No route matched ${req.method} ${url}`)
     );
-const main: EntryPoint = async (req) => switcher(parseURL(req).pathname, req);
 
-export const root = join(Deno.cwd(), "docs");
+const main: EntryPoint = async (req) => switcher(parseURL(req).pathname, req);
 
 initDB().then(
     (amount) => {

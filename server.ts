fce6cc34125cd607c3eb5e4059cf0efc4895997b
Hexiang <qihexiang@outlook.com>
2022-03-05 23:49:41 +0800

    Move fallback handler out

--- server.ts
+++ server.ts
@@ -1,5 +1,4 @@
 import {
-    createRes,
     createSwRtX,
     EntryPoint,
     Get,
@@ -8,22 +7,21 @@ import {
 } from "https://deno.land/x/freesia@v1.0.8/mod.ts";
 import { join } from "https://deno.land/std@0.127.0/path/mod.ts";
 import { serve } from "https://deno.land/std@0.127.0/http/server.ts";
-import { Status } from "https://deno.land/std@0.127.0/http/http_status.ts";
 import fileHandler from "./handlers/fileHandler.ts";
 import searchHandler from "./handlers/serachHandler.ts";
 import listHandler from "./handlers/listHandler.ts";
 import init from "./init.ts";
+import ticketHandler from "./handlers/ticketHandler.ts";
+import fallbackHandler from "./handlers/fallbackHandler.ts";
 
-export const token = crypto.randomUUID();
 export const root = join(Deno.cwd(), "docs");
 
 const { switcher } = createSwRtX
     .route("/file/<uuid>/<filepath>", Get(fileHandler))
     .route("/search", Get(searchHandler))
     .route("/list/<type>", Get(listHandler))
-    .fallback(async (url, req) =>
-        createRes(Status.NotFound, `No route matched ${req.method} ${url}`)
-    );
+    .route("/ticket", Get(ticketHandler))
+    .fallback(fallbackHandler);
 
 const main: EntryPoint = async (req) => switcher(parseURL(req).pathname, req);
 
@@ -31,4 +29,3 @@ init();
 serve(shimHTTP(main), {
     port: 8000,
 });
-

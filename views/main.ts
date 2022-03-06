a937fc6f1ff5d9395276644ccccad7a86cdabb95
Hexiang <qihexiang@outlook.com>
2022-03-06 12:08:03 +0800

    use deno.ns and dom as lib

--- views/main.ts
+++ views/main.ts
@@ -1,5 +1,4 @@
 import ReactDOM from "https://esm.sh/react-dom@17.0.2";
 import App from "./App.tsx";
 
-// @ts-ignore: This file executed in browser only.
 ReactDOM.render(App({ url: "/", method: "GET" }), document.getElementById("App"));

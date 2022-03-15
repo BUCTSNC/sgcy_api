4fd67c469aef84d48b49b3d086d43a04738562ed
Hexiang Qi <qihexiang@outlook.com>
2022-03-15 22:20:14 +0800

    Support watching files in ./views

--- generateMainJS.ts
+++ generateMainJS.ts
@@ -1,34 +1,60 @@
-// @deno-types: "https://deno.land/x/esbuild@v0.14.25/mod.ts";
 import * as esbuild from "https://deno.land/x/esbuild@v0.14.25/mod.js";
+import {createWrapper} from "https://deno.land/x/freesia@v1.1.2/mod.ts"
 
-console.log("started");
+const watcher = Deno.watchFs("./views", { recursive: true });
+console.log("Watcher created.");
 
-export const mainJS = await Deno.emit("./views/main.tsx", {
-    bundle: "module",
-    compilerOptions: {
-        inlineSourceMap: true,
-        target: "esnext",
-        module: "esnext",
-        lib: ["dom", "dom.iterable", "dom.asynciterable", "deno.ns"],
-        sourceMap: true,
-        removeComments: true,
-    },
+const fsEventHandler = () => {
+    console.log(`Rebuild main.js`);
+    return Deno.emit("./views/main.tsx", {
+        bundle: "module",
+        compilerOptions: {
+            inlineSourceMap: true,
+            target: "esnext",
+            module: "esnext",
+            lib: ["dom", "dom.iterable", "dom.asynciterable", "deno.ns"],
+            sourceMap: true,
+            removeComments: true,
+        },
+    })
+        .then((result) => result.files["deno:///bundle.js"])
+        .then((mainJS) =>
+            esbuild.transform(mainJS, {
+                minify: true,
+                minifyWhitespace: true,
+                minifyIdentifiers: true,
+                minifySyntax: true,
+                format: "esm",
+                treeShaking: true,
+            })
+        )
+        .then((content) => {
+            return Deno.writeTextFile("static/main.js", content.code);
+        });
+};
+
+const sleep = <T>(timeout: number, data?: T) => new Promise<T | undefined>((res, _rej) => {
+    setTimeout(() => res(data), timeout)
 })
-    .then((result) => result.files["deno:///bundle.js"]);
 
-console.log("emitted");
+let updatedFlag = Symbol();
 
-await esbuild.transform(mainJS, {
-    minify: true,
-    minifyWhitespace: true,
-    minifyIdentifiers: true,
-    minifySyntax: true,
-    format: "esm",
-    treeShaking: true,
-}).then((content) => {
-    return Deno.writeTextFile("static/main.js", content.code);
-});
+const throttle = (waitToExec: number) => createWrapper<typeof fsEventHandler, (event: Deno.FsEvent) => Promise<void>>(
+    async (event) => {
+        console.log(`${event.kind} ${event.paths}.`)
+        const currentSymbol = Symbol();
+        updatedFlag = currentSymbol;
+        await sleep(waitToExec);
+        if(currentSymbol === updatedFlag) return [[], async (_void) => {
+            await _void;
+            console.log("Rebuild finished.")
+        }]
+        else return [null, async () => {}]
+    }
+)
 
-// await Deno.writeTextFile("static/main.js", mainJS);
+const throttled = throttle(1000)(fsEventHandler)
 
-Deno.exit(0);
+for await (const event of watcher) {
+    throttled(event)
+}

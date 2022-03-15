657e33856bf74741cd8977a543082ab6c8eb6763
Hexiang Qi <qihexiang@outlook.com>
2022-03-15 22:29:12 +0800

    添加了代码说明

--- generateMainJS.ts
+++ generateMainJS.ts
@@ -37,18 +37,21 @@ const sleep = <T>(timeout: number, data?: T) => new Promise<T | undefined>((res,
     setTimeout(() => res(data), timeout)
 })
 
-let updatedFlag = Symbol();
+let lastEvent: Deno.FsEvent
 
 const throttle = (waitToExec: number) => createWrapper<typeof fsEventHandler, (event: Deno.FsEvent) => Promise<void>>(
     async (event) => {
         console.log(`${event.kind} ${event.paths}.`)
-        const currentSymbol = Symbol();
-        updatedFlag = currentSymbol;
+        // 将本次事件绑定到lastEvent上
+        lastEvent = event;
+        // 等待若干时常
         await sleep(waitToExec);
-        if(currentSymbol === updatedFlag) return [[], async (_void) => {
+        // lastEvent在等待期间没有被更新，调用函数重建main.js
+        if(event === lastEvent) return [[], async (_void) => {
             await _void;
             console.log("Rebuild finished.")
         }]
+        // lastEvent在等待期间被刷新了，让后续事件来调用函数
         else return [null, async () => {}]
     }
 )

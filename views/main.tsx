c0b118e4a5b8292127698fe77cbeccabff33b7be
Hexiang Qi <qihexiang@outlook.com>
2022-03-15 22:21:25 +0800

    Remove ts-ignore comment as use dom lib

--- views/main.tsx
+++ views/main.tsx
@@ -5,6 +5,5 @@ ReactDOM.hydrate(
     <BrowserRouter>
         <App hotList={{ daily: [], weekly: [], monthly: [], yearly: [] }} />
     </BrowserRouter>,
-    // @ts-ignore: this file will not use in deno.
     document.getElementById("App"),
 );

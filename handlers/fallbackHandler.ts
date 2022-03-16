235b985435ed637bddaf80ac6a6ca7928dacd655
Hexiang Qi <qihexiang@outlook.com>
2022-03-16 09:09:15 +0800

    Use SSRHandler as fallback

--- handlers/fallbackHandler.ts
+++
@@ -1,6 +0,0 @@
-import { httpStatus } from "../deps/std.ts";
-import { createRes } from "../deps/freesia.ts";
-
-const fallbackHandler = async (url: string, req: Request) =>
-    createRes(httpStatus.NotFound, `No route matched ${req.method} ${url}`);
-export default fallbackHandler;

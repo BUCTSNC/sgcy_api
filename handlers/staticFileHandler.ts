4cfd0967650e8bc8ba1c17dbc6b31029d399a448
Hexiang Qi <qihexiang@outlook.com>
2022-03-18 15:03:50 +0800

    Load css files into template

--- handlers/staticFileHandler.ts
+++ handlers/staticFileHandler.ts
@@ -2,11 +2,27 @@ import { httpStatus, path, readableStreamFromReader } from "../deps/std.ts";
 import { compute, createRes, Respond } from "../deps/freesia.ts";
 import { lookup } from "../deps/mediaTypes.ts";
 import { staticRoot } from "../constant.ts";
-import { __dirname } from "../utils/parseImportUrl.ts";
+import { __root } from "../utils/parseImportUrl.ts";
 
 const { join, extname } = path;
 
-export const indexHTML = await Deno.readTextFile(join(__dirname(import.meta.url), '..', 'index.html'));
+const preloadStylesheets = async () => {
+    let cssFiles: string[] = [];
+    for await (const entry of Deno.readDir(join(staticRoot, "stylesheets"))) {
+        if (entry.isFile && (/(.css)$/).exec(entry.name)) {
+            cssFiles = [
+                ...cssFiles,
+                `<link href="/static/stylesheets/${entry.name}" rel="stylesheet" type="text/css" />`,
+            ];
+        }
+    }
+    return cssFiles.join("\n");
+};
+
+export const indexHTML = await Deno.readTextFile(join(__root, "index.html"))
+    .then(async (content) =>
+        content.replace("<!-- STYLES -->", await preloadStylesheets())
+    );
 
 export const indexHandler = async () =>
     createRes(indexHTML, ["Content-Type", "text/html; charset=UTF-8"]);

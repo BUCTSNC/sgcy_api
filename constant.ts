fbc31f68bfe4661d1e0ad475790f97fd3e72ab86
Hexiang Qi <qihexiang@outlook.com>
2022-03-18 09:27:25 +0800

    Use __dirname function to get staticRoot

--- constant.ts
+++ constant.ts
@@ -1,6 +1,7 @@
 import { path } from "./deps/std.ts";
+import { __dirname } from "./utils/parseImportUrl.ts";
 const { join } = path;
 
-export const staticRoot = join(Deno.cwd(), "static");
-export const root = join(Deno.cwd(), "docs");
+export const staticRoot = join(__dirname(import.meta.url), "static");
+export const root = join(__dirname(import.meta.url), "docs");
 export const listen = 8000;

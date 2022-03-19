aa95111e589957450c4fb4caf4d44788f58a20e2
Hexiang <qihexiang@outlook.com>
2022-03-19 09:24:15 +0800

    Pin all react dependencies to esm v70

--- deps/react.ts
+++ deps/react.ts
@@ -1,16 +1,16 @@
 // 前端调试时，再所有URL末尾增加?dev参数来获得带有开发工具的文件。
 // 所有的React相关库必须同时使用调试版本或非调试版本。
 // 使用开发版本bundule并minified的main.js大约500kb，比最优化版本大200kb左右
+// @ts-nocheck: "Bad types"
 
 export {
     default as React,
     useEffect,
     useState,
-} from "https://esm.sh/react@17.0.2?dev";
-export { default as ReactDOM } from "https://esm.sh/react-dom@17.0.2?dev";
-export { renderToString } from "https://esm.sh/react-dom@17.0.2/server?dev";
+} from "https://esm.sh/react@17.0.2?dev&pin=v70";
+export { default as ReactDOM } from "https://esm.sh/react-dom@17.0.2?dev&pin=v70";
+export { renderToString } from "https://esm.sh/react-dom@17.0.2/server?dev&pin=v70";
 export {
-    BrowserRouter,
     Outlet,
     Route,
     Routes,
@@ -18,6 +18,5 @@ export {
     useNavigate,
     useSearchParams,
     useLocation
-} from "https://esm.sh/react-router-dom@6.2.2?dev";
-export { StaticRouter } from "https://esm.sh/react-router-dom@6.2.2/server?dev&deps=@types/react@17.0.39";
-export { createUseStyles } from "https://esm.sh/react-jss@10.9.0?dev&deps=@types/react@17.0.39";
+} from "https://esm.sh/react-router-dom@6.2.2?dev&pin=v70";
+export { StaticRouter } from "https://esm.sh/react-router-dom@6.2.2/server?dev&pin=v70";
